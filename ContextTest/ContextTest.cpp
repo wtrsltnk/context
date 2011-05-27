31c04ee8ce9c249559070cdda1ed78e95eab7445
Wouter Saaltink <wouter.saaltink@gmail.com>
2011-05-27 18:50:57 +0000

    project settings
    git-svn-id: svn://localhost:8888/Context@900 45aaaf3f-5fed-41db-b156-2e14097e5472

--- ContextTest/ContextTest.cpp
+++ ContextTest/ContextTest.cpp
@@ -19,30 +19,67 @@ ContextTest::~ContextTest()
 		delete this->mShader;
 }
 
+unsigned int vboid[2];
 bool ContextTest::onInitializeGl()
 {
+	GLfloat vertices[] = {
+		0.0f, 0.0f, -10.0f,
+		0.0f, 5.0f, -10.0f,
+		5.0f, 5.0f, -10.0f
+	};
+	GLuint indices[] = {
+		0, 1, 2
+	};
 	this->mShader = new DefaultShader();
 	
+	if (GLEE_ARB_vertex_buffer_object)
+	{
+		glGenBuffersARB(2, vboid);
+		
+		glBindBufferARB(GL_ARRAY_BUFFER_ARB, vboid[0]);
+		glBufferDataARB(GL_ARRAY_BUFFER_ARB, sizeof(vertices), vertices, GL_STATIC_DRAW_ARB);
+		
+		glBindBufferARB(GL_ELEMENT_ARRAY_BUFFER_ARB, vboid[1]);
+		glBufferDataARB(GL_ELEMENT_ARRAY_BUFFER_ARB, sizeof(indices), indices, GL_STATIC_DRAW_ARB);
+	}
 	return true;
 }
 
 void ContextTest::onIdle(const GameTime* gameTime)
 {
-	GLfloat vertices[] ={
-		0.0f, 0.0f, -10.0f,
-		0.0f, 5.0f, -10.0f,
-		5.0f, 5.0f, -10.0f
-	};
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
 	this->mShader->use();
 	this->mShader->setProjectionMatrix(this->mProjection);
 	this->mShader->setModelviewMatrix(this->mModelview);
 	
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glVertexPointer(3, GL_FLOAT, sizeof(GL_FLOAT) * 3, &vertices);
+	if (GLEE_ARB_vertex_buffer_object)
+	{
+		glBindBufferARB(GL_ARRAY_BUFFER_ARB, vboid[0]);
+		glBindBufferARB(GL_ELEMENT_ARRAY_BUFFER_ARB, vboid[1]);
 
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glVertexPointer(3, GL_FLOAT, 0, 0);
+
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+
+		glDisableClientState(GL_VERTEX_ARRAY);
+
+		glBindBufferARB(GL_ARRAY_BUFFER_ARB, 0);
+		glBindBufferARB(GL_ELEMENT_ARRAY_BUFFER_ARB, 0);
+	}
+	else
+	{
+		GLfloat vertices[] ={
+			0.0f, 0.0f, -10.0f,
+			0.0f, 5.0f, -10.0f,
+			5.0f, 5.0f, -10.0f
+		};
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glVertexPointer(3, GL_FLOAT, sizeof(GL_FLOAT) * 3, &vertices);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+	}
 }
 
 void ContextTest::onResize(int w, int h)
--- ContextTest/nbproject/Makefile-variables.mk
+++ ContextTest/nbproject/Makefile-variables.mk
@@ -7,37 +7,37 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # linuxDebug configuration
-CND_PLATFORM_linuxDebug=MinGW-Linux-x86
-CND_ARTIFACT_DIR_linuxDebug=dist/linuxDebug/MinGW-Linux-x86
+CND_PLATFORM_linuxDebug=GNU-Linux-x86
+CND_ARTIFACT_DIR_linuxDebug=dist/linuxDebug/GNU-Linux-x86
 CND_ARTIFACT_NAME_linuxDebug=contexttest
-CND_ARTIFACT_PATH_linuxDebug=dist/linuxDebug/MinGW-Linux-x86/contexttest
-CND_PACKAGE_DIR_linuxDebug=dist/linuxDebug/MinGW-Linux-x86/package
+CND_ARTIFACT_PATH_linuxDebug=dist/linuxDebug/GNU-Linux-x86/contexttest
+CND_PACKAGE_DIR_linuxDebug=dist/linuxDebug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_linuxDebug=contexttest.tar
-CND_PACKAGE_PATH_linuxDebug=dist/linuxDebug/MinGW-Linux-x86/package/contexttest.tar
+CND_PACKAGE_PATH_linuxDebug=dist/linuxDebug/GNU-Linux-x86/package/contexttest.tar
 # linuxRelease configuration
-CND_PLATFORM_linuxRelease=MinGW-Linux-x86
-CND_ARTIFACT_DIR_linuxRelease=dist/linuxRelease/MinGW-Linux-x86
+CND_PLATFORM_linuxRelease=GNU-Linux-x86
+CND_ARTIFACT_DIR_linuxRelease=dist/linuxRelease/GNU-Linux-x86
 CND_ARTIFACT_NAME_linuxRelease=contexttest
-CND_ARTIFACT_PATH_linuxRelease=dist/linuxRelease/MinGW-Linux-x86/contexttest
-CND_PACKAGE_DIR_linuxRelease=dist/linuxRelease/MinGW-Linux-x86/package
+CND_ARTIFACT_PATH_linuxRelease=dist/linuxRelease/GNU-Linux-x86/contexttest
+CND_PACKAGE_DIR_linuxRelease=dist/linuxRelease/GNU-Linux-x86/package
 CND_PACKAGE_NAME_linuxRelease=contexttest.tar
-CND_PACKAGE_PATH_linuxRelease=dist/linuxRelease/MinGW-Linux-x86/package/contexttest.tar
+CND_PACKAGE_PATH_linuxRelease=dist/linuxRelease/GNU-Linux-x86/package/contexttest.tar
 # win32Debug configuration
-CND_PLATFORM_win32Debug=MinGW-Windows
-CND_ARTIFACT_DIR_win32Debug=dist/win32Debug/MinGW-Windows
+CND_PLATFORM_win32Debug=GNU-Windows
+CND_ARTIFACT_DIR_win32Debug=dist/win32Debug/GNU-Windows
 CND_ARTIFACT_NAME_win32Debug=contexttest
-CND_ARTIFACT_PATH_win32Debug=dist/win32Debug/MinGW-Windows/contexttest
-CND_PACKAGE_DIR_win32Debug=dist/win32Debug/MinGW-Windows/package
+CND_ARTIFACT_PATH_win32Debug=dist/win32Debug/GNU-Windows/contexttest
+CND_PACKAGE_DIR_win32Debug=dist/win32Debug/GNU-Windows/package
 CND_PACKAGE_NAME_win32Debug=contexttest.tar
-CND_PACKAGE_PATH_win32Debug=dist/win32Debug/MinGW-Windows/package/contexttest.tar
+CND_PACKAGE_PATH_win32Debug=dist/win32Debug/GNU-Windows/package/contexttest.tar
 # win32Release configuration
-CND_PLATFORM_win32Release=MinGW-Windows
-CND_ARTIFACT_DIR_win32Release=dist/win32Release/MinGW-Windows
+CND_PLATFORM_win32Release=GNU-Windows
+CND_ARTIFACT_DIR_win32Release=dist/win32Release/GNU-Windows
 CND_ARTIFACT_NAME_win32Release=contexttest
-CND_ARTIFACT_PATH_win32Release=dist/win32Release/MinGW-Windows/contexttest
-CND_PACKAGE_DIR_win32Release=dist/win32Release/MinGW-Windows/package
+CND_ARTIFACT_PATH_win32Release=dist/win32Release/GNU-Windows/contexttest
+CND_PACKAGE_DIR_win32Release=dist/win32Release/GNU-Windows/package
 CND_PACKAGE_NAME_win32Release=contexttest.tar
-CND_PACKAGE_PATH_win32Release=dist/win32Release/MinGW-Windows/package/contexttest.tar
+CND_PACKAGE_PATH_win32Release=dist/win32Release/GNU-Windows/package/contexttest.tar
 #
 # include compiler specific variables
 #
